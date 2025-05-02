178c0c6122c1f173d390fbfaed1586ee5e16cd49
fahmirzl <fahmirizal.dev@gmail.com>
2025-05-02 21:23:05 +0700

    feat(button): create button component with style

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,25 @@
+import styles from './button.module.css'
+
+type Button = {
+    onClick?: () => void,
+    children: React.ReactNode,
+    width?: string,
+    height?: string,
+    color?: string,
+    bgColor?: string,
+    radius?: string,
+}
+
+const Button = (button: Button) => {
+    return (
+        <button className={styles.button} onClick={button.onClick} style={{ 
+            width: button.width ?? "100px",
+            height: button.height ?? "30px",
+            color: button.color ?? "#fff",
+            backgroundColor: button.bgColor ?? "#000",
+            borderRadius: button.radius ?? "0"
+         }}><span>{button.children}</span></button>
+    )
+}
+
+export default Button
